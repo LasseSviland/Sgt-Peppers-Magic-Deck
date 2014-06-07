30497014cb8cbde7813447256ce9c08d7e1e06ed
Lasse Sviland <lassesviland@hotmail.com>
2014-06-07 18:57:29 -0400

    Made the path for the seed file relative

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-File.readlines("/Users/apprentice/Desktop/Sgt-Peppers-Magic-Deck/db/beatles.txt").each do |line|
+beatles_file_path = File.expand_path("../beatles.txt", __FILE__)
+File.readlines(beatles_file_path).each do |line|
   entry = line.split("|")
   card = Card.new(question: entry[0], answer: entry[1].chomp, deck_id: 1)
   card.save

128fb07bd1d712b5e1dc8e9b4bd636a98a63cf6a
gooby pls <raghav1987@gmail.com>
2014-06-08 13:14:37 -0400

    update guess class

--- db/migrate/20140606172702_create_guesses.rb
+++ db/migrate/20140606172702_create_guesses.rb
@@ -2,6 +2,7 @@ class CreateGuesses < ActiveRecord::Migration
   def change
     create_table :guesses do |t|
       t.integer :tries, default: 0
+      t.boolean :guessed, default: false 
       t.belongs_to :round
 
       t.timestamps
